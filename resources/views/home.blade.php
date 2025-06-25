3aaae5d358e719009eb69165d0c4da788fa60067
MaxStill007 <elvis.temirov@gmail.com>
2025-06-25 18:18:33 +0300

    v1.1.72
    переделал встречное сообщение

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container bg-CustomDark">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card bg-CustomBorder">
+                <div class="card-header">
+                    <h4 class="mb-0 text-CustomWhite headlines">{{ __('Личный кабинет') }}</h4>
+                </div>
 
-                <div class="card-body">
+                <div class="card-body text-CustomWhite">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Вы вошли в систему!') }}
                 </div>
             </div>
         </div>
